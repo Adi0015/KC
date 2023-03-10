3f72890de95bc05284098fb4fb4554235275653e
Aditya gohil <gohiladityaa@gmail.com>
2023-03-10 14:59:41 +0530

    Contact-us DB done

--- models/enquiry.js
+++ models/enquiry.js
@@ -1,18 +1,18 @@
 const db = require('../lib/db')
 
 class Enquiry {
-    constructor(parent_name,
-        parent_email,
-        parent_phone,
-        message) {
+    constructor(parent_name, parent_email, parent_phone, message) {
         this.parent_name = parent_name
         this.parent_email = parent_email
         this.parent_phone = parent_phone
         this.message = message
     }
-    async save(){
-        let sql=`INSERT INTO enquiry(parent_name, email, phone, message) VALUES ('${this.parent_name}','${this.parent_email}','${this.parent_phone}','${this.message}')`
+
+    async save() {
+        const currentDate = new Date().toISOString().slice(0, 19).replace('T', ' ')
+        const sql = `INSERT INTO enquiry(parent_name, email, phone, message, Date) VALUES ('${this.parent_name}', '${this.parent_email}', '${this.parent_phone}', '${this.message}', '${currentDate}')`
         return await db.execute(sql)
     }
 }
+
 module.exports = Enquiry
